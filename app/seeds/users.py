50a33b414b32a44126e9d762f99220d25af5ffd9
Big-Hdub <hdub-bb@hotmail.com>
2024-07-24 12:12:14 -0700

    updating user seeder

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -2,18 +2,23 @@ from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
 
 
+data=[{
+    'username':'Demouser', 'email':'demo@aa.io', 'password':'password', 'age':30, 'first_name':'Demo', 'last_name':'User'
+    },
+    {
+    'username':'MarnieS', 'email':'marnie@aa.io', 'password':'password', 'age':12, 'first_name':'Marnie', 'last_name':'Smith'
+    },
+    {
+    'username':'BobbieS', 'email':'bobbie@aa.io', 'password':'password', 'age':45, 'first_name':'Bobbie', 'last_name':'Smith'
+    },
+    ]
+
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password', age=30, first_name='Demo', last_name='User')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', age=12, first_name='Marnie', last_name='Smith')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password', age=45, first_name='Bobbie', last_name='Smith')
+    for info in data:
+        user = User(info)
+        db.session.add(user)
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
     db.session.commit()
 
 
