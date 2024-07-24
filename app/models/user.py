8fc30d0da4af7536c094770870f2a865c17229c2
Big-Hdub <hdub-bb@hotmail.com>
2024-07-23 22:01:42 -0700

    updated user model

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,9 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Integer, nullable=False)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
@@ -28,6 +31,7 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
+            'name': self.first_name+' '+self.last_name,
             'username': self.username,
             'email': self.email
         }
