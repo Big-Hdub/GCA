32df248a33609149b6db5ded8fa3872ed9a2d662
Big-Hdub <hdub-bb@hotmail.com>
2024-07-27 14:22:16 -0700

    fixed some css and modified landing page verses.

--- react-vite/src/components/LandingPage/Section1.jsx
+++ react-vite/src/components/LandingPage/Section1.jsx
@@ -2,15 +2,15 @@ export default function Section1() {
     return (
         <div id="farmer-quotes-container" className="flex acenter around">
             <div className="quote-divs flex column">
-                <p>&quot;For god so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.&quot;</p>
-                <p className="tend">John 3:16</p>
+                <p>&quot;For god so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.  For god did not send his Son into the world to condemn the world, but to save the world through him.&quot;</p>
+                <p className="tend">John 3:16-17</p>
             </div>
             <div>
                 <div id="farmer-logo"></div>
             </div>
             <div className="quote-divs flex column">
-                <p>&quot;For the Spirit God gave us does not make us timid, but gives us power, love and self-discipline.&quot;</p>
-                <p className="tend">2 Timothy 1:7</p>
+                <p>&quot;He has saved us and called us to a holy life - not because of anything we have done but because of his own purpose and grace. This grace was given us in Christ Jesus before the beginning of time, but it has now been revealed through the appearing of our Saviour, Christ Jesus, who has destroyed death and has brought life and immortality to light through the gospel.&quot;</p>
+                <p className="tend">2 Timothy 1:9-10</p>
             </div>
         </div>
     )
